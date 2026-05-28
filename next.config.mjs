22281be2f06262cd98ad0866f89596c792cbd4b3
itsmsjwork-ux <its.msj.work@gmail.com>
2026-05-28 22:17:29 +0530

    fix vercel config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig = {
-  output: "export",
-  basePath: isGithubPages ? "/MSJATTAR" : "",
-  assetPrefix: isGithubPages ? "/MSJATTAR/" : "",
   images: {
     unoptimized: true
   }
